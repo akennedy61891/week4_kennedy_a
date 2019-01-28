f0df098f450f1af9a190011fc59a52b06d8e3a61
akennedy61891 <a_kennedy61891@fanshaweonline.ca>
2019-01-28 13:34:03 -0500

    almost working game

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -1,21 +1,51 @@
 (() => {
 	console.log('js is linked up');
 
-	
-
 	//window.addEventListener('load', changeHeadline);
 
-	function changeHeadline() {
-		document.querySelector('h1').textContent = "Hey There";
-		document.querySelector('p').textContent = "How's it goin?";
+	//function changeHeadline() {
+	//	document.querySelector('h1').textContent = "Hey There";
+	//	document.querySelector('p').textContent = "How's it goin?";
+	//}
+
+	//const theButton = document.querySelector('#buttonHolder img');
+
+	//theButton.addEventListener('click', changeHeadline);
+	// set up the puzzle pieces and boards
+	
+	const thePieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"]; 
+
+	// get a reference to the drag slide
+	let piecesBoard = document.querySelector(".puzzle-pieces"); 
+	let puzzleBoard = document.querySelector(".puzzle-board");
+
+	//get a reference to the buttons at the bottom so we can change the puzzle
+	let puzzleSelectors = document.querySelectorAll("#buttonHolder img"); 
+
+	// functions go inthe middle 
+	function createPuzzlePieces(pictureIndex) {
+		//generate images here --> need to make 4 (top left, top right, bottom right, bottom left)
+		//debugger;
+		thePieces.forEach((piece, index) => {
+			let newPuzzlePiece = `<img id="piece${index}" class="puzzle-image"
+				src="images/${piece + pictureIndex}.jpg" alt="puzzle piece">`;
+
+			piecesBoard.innerHTML += newPuzzlePiece;
+		});
 	}
 
-	const theButton = document.querySelector('#buttonHolder img');
+	function resetPuzzlePieces() {
+		//change the current puzzle, regenerate the pieces
+		//debugger;
+		piecesBoard.innerHTML = "";
 
-	theButton.addEventListener('click', changeHeadline);
-	// set up the puzzle pieces and boards
-	//
 
+		createPuzzlePieces(this.dataset.puzzleref);
+	}
 
+	// event handling goes here 
+	puzzleSelectors.forEach(button => button.addEventListener("click", resetPuzzlePieces));
+
+	createPuzzlePieces(0);
 
 })();
